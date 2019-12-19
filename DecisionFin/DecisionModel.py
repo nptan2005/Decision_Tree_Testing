a8b4fd79b931d8655026aad8f109ca837c3c42ac
PAD Nguyễn <nptan2005@gmail.com>
2019-12-19 18:38:13 +0700

    https://github.com/nptan2005/Decision_Tree_Testing.git

---
+++ DecisionFin/DecisionModel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+class DecisionModel:
+
+    def __init__(self):
+        
---
+++ DecisionFin/PrepareData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class PrepareData:
+
+    def __init__(self):
+        self._data = pd.read_csv('Data/DECISION_TREE_TRAIN_DATA_3.csv',index_col='CUSTOMER_ID')
